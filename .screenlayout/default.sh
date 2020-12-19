3d29b78cd69af4591f19820a4b3a00474edc96d4
StrixSC <talon.strix@gmail.com>
2020-12-19 16:52:51 -0500

    added arch-install file that contains commands to execute to easily install arch + screenlayout configs & wakeup configs

---
+++ .screenlayout/default.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-D-1 --primary --mode 1920x1080 --pos 2560x1080 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 640x0 --rotate inverted --output DP-1 --mode 2560x1440 --pos 0x1080 --rotate normal
