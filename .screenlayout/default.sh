ae6ec13a5a86bcff10f6e0fc701696cabbfe63a0
StrixSC <talon.strix@gmail.com>
2020-12-20 15:22:25 -0500

    Added screen refresh rate to default screenlayout config & added picom/compton configs

--- .screenlayout/default.sh
+++ .screenlayout/default.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DVI-D-1 --primary --mode 1920x1080 --pos 2560x1080 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 640x0 --rotate inverted --output DP-1 --mode 2560x1440 --pos 0x1080 --rotate normal
+xrandr --output DVI-D-1 --mode 1920x1080 --rate 144 --pos 2560x1080 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 640x0 --rotate inverted --output DP-1 --mode 2560x1440 --primary --pos 0x1080 --rotate normal --rate 144
