ee0aaefffef5a9a8a041cda6f69864fdaeff2f38
StrixSC <talon.strix@gmail.com>
2021-01-03 04:17:20 -0500

    Switched to nvidia drivers so needed to switch default layout & refresh rate

--- .screenlayout/default.sh
+++ .screenlayout/default.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DVI-D-1 --mode 1920x1080 --rate 144 --pos 2560x1080 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 640x0 --rotate inverted --output DP-1 --mode 2560x1440 --primary --pos 0x1080 --rotate normal --rate 144
+xrandr --output DVI-D-0 --mode 1920x1080 --rate 144 --pos 2560x1080 --rotate normal --output HDMI-0 --mode 1920x1080 --pos 320x0 --rotate inverted --output DP-0 --primary --mode 2560x1440 --pos 0x1080 --rotate normal --rate 144 
